d3518826220b7568d988c5d67c56720b87d5fe31
Himanshu Gupta <himanshu@knoldus.com>
2016-05-15 01:19:28 +0530

    created a dynamic stack class using interfaces

--- chapter9/IFTest.java
+++ chapter9/IFTest.java
@@ -1,20 +1,20 @@
-class Stack {
+class FixedStack implements IntStack {
   private int stck[];
   private int tos;
 
-  Stack(int size) {
+  FixedStack(int size) {
     stck = new int[size];
     tos = -1;
   }
 
-  void push(int item) {
+  public void push(int item) {
     if(tos == stck.length-1)
       System.out.println("Stack is full");
     else
       stck[++tos] = item;
   }
 
-  int pop() {
+  public int pop() {
     if(tos < 0) {
       System.out.println("Stack underflow");
       return 0;
@@ -24,10 +24,10 @@ class Stack {
   }
 }
 
-class TestStack2 {
+class IFTest {
   public static void main(String args[]) {
-    Stack myStack1 = new Stack(5);
-    Stack myStack2 = new Stack(8);
+    FixedStack myStack1 = new FixedStack(5);
+    FixedStack myStack2 = new FixedStack(8);
 
     for(int i=0; i<5; i++) myStack1.push(i);
     for(int i=0; i<8; i++) myStack2.push(i);
---
+++ chapter9/IFTest2.java
@@ -0,0 +1,44 @@
+class DynStack implements IntStack {
+  private int stck[];
+  private int tos;
+
+  DynStack(int size) {
+    stck = new int[size];
+    tos = -1;
+  }
+
+  public void push(int item) {
+    if(tos == stck.length-1) {
+      int temp[] = new int[stck.length*2];
+      for(int i=0; i<stck.length-1; i++) temp[i] = stck[i];
+      stck = temp;
+    }
+
+    stck[++tos] = item;
+  }
+
+  public int pop() {
+    if(tos < 0) {
+      System.out.println("Stack underflow");
+      return 0;
+    } else {
+      return stck[tos--];
+    }
+  }
+}
+
+class IFTest2 {
+  public static void main(String args[]) {
+    DynStack myStack1 = new DynStack(5);
+    DynStack myStack2 = new DynStack(8);
+
+    for(int i=0; i<12; i++) myStack1.push(i);
+    for(int i=0; i<10; i++) myStack2.push(i);
+
+    System.out.println("Stack in myStack1:");
+    for(int i=0; i<12; i++) System.out.println(myStack1.pop());
+
+    System.out.println("Stack in myStack2:");
+    for(int i=0; i<20; i++) System.out.println(myStack2.pop());
+  }
+}
