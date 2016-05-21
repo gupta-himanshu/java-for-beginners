c6f961ce9b2a04ce15c059e5a13d35cd266c9c8c
Himanshu Gupta <himanshu@knoldus.com>
2016-05-22 00:08:20 +0530

    an example of multiple catch statements

---
+++ chapter10/MultiCatch.java
@@ -0,0 +1,16 @@
+class MultiCatch {
+  public static void main(String args[]) {
+    try {
+      int a = args.length;
+      System.out.println("a = " + a);
+      int b = 42 / a;
+      int c[] = { 1 };
+      c[42] = 99;
+    } catch (ArithmeticException e) {
+      System.out.println("Division by 0: " + e);
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("Array index oob: " + e);
+    }
+    System.out.println("After try/catch block.");
+  }
+}
