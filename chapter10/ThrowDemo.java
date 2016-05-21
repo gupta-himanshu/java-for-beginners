84fc05fb64b0bef9a1b8f3c81d1e67f28728c185
Himanshu Gupta <himanshu@knoldus.com>
2016-05-22 00:34:05 +0530

    example of throw statement in java

---
+++ chapter10/ThrowDemo.java
@@ -0,0 +1,18 @@
+class ThrowDemo {
+  static void demoProc() {
+    try {
+      throw new NullPointerException("demo");
+    } catch (NullPointerException e) {
+      System.out.println("Caught inside demoProc.");
+      throw e;
+    }
+  }
+
+  public static void main(String args[]) {
+    try {
+      demoProc();
+    } catch (NullPointerException e) {
+      System.out.println("Recaught: " + e);
+    }
+  }
+}
