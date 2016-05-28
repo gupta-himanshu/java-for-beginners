a624bb102a0b4a1389eab0ace5f44923d66ae84d
Himanshu Gupta <himanshu@knoldus.com>
2016-05-29 00:40:23 +0530

    added a demo of 2 thread example in java

---
+++ chapter11/ThreadDemo.java
@@ -0,0 +1,37 @@
+class NewThread implements Runnable {
+  Thread t;
+
+  NewThread() {
+    t = new Thread(this, "Demo Thread");
+    System.out.println("Child Thread: " + t);
+    t.start();
+  }
+
+  public void run() {
+    try {
+      for(int i=5; i>0; i--) {
+        System.out.println("Child Thread: " + i);
+        Thread.sleep(500);
+      }
+    } catch (InterruptedException e) {
+        System.out.println("Child Interrupted.");
+    }
+    System.out.println("Exiting Child Thread.");
+  }
+}
+
+class ThreadDemo {
+  public static void main(String args[]) {
+    new NewThread();
+
+    try {
+      for(int i=5; i>0; i--) {
+        System.out.println("Main Thread: " + i);
+        Thread.sleep(500);
+      }
+    } catch (InterruptedException e) {
+        System.out.println("Main Thread Interrupted.");
+    }
+    System.out.println("Exiting Main Thread.");
+  }
+}
