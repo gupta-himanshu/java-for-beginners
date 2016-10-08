c3b116ab0e61cfda0be8de9720ae24a5b921be14
Himanshu Gupta <himanshu@knoldus.com>
2016-10-08 20:49:06 +0530

    added an example of string concatenation in java

---
+++ string-handling/ConCat.java
@@ -0,0 +1,10 @@
+class ConCat {
+  public static void main(String args[]) {
+    String longStr = "This could have been " +
+      "a very long line that would have " +
+      "wrapped around. But string concatenation " +
+      "prevent this.";
+
+    System.out.println(longStr);
+  }
+}
