06102ee058fd7f65fdb5d29a23ab9cf6dbd3f6b9
Himanshu Gupta <himanshu@knoldus.com>
2016-10-11 20:19:23 +0530

    created a sample for reverse of stringbuffer in java

---
+++ string-handling/ReverseDemo.java
@@ -0,0 +1,9 @@
+class ReverseDemo {
+  public static void main(String args[]) {
+    StringBuffer sb = new StringBuffer("abcdef");
+
+    System.out.println(sb);
+    sb.reverse();
+    System.out.println(sb);
+  }
+}
