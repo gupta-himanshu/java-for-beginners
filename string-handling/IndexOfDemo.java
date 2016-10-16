ee071d1c32c98a67dee5a89d5e32a6c9f6ed57af
Himanshu Gupta <himanshu@knoldus.com>
2016-10-16 20:08:03 +0530

    created a sample for index demo in java string

---
+++ string-handling/IndexOfDemo.java
@@ -0,0 +1,12 @@
+class IndexOfDemo {
+  public static void main(String args[]) {
+    StringBuffer sb = new StringBuffer("one two one");
+    int i;
+
+    i = sb.indexOf("one");
+    System.out.println("First index: " + i);
+
+    i = sb.lastIndexOf("one");
+    System.out.println("Last Index: " + i);
+  }
+}
